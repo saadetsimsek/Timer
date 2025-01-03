c8c04bb43f3d5b36b59daa0ee80cd237d64fa7e6
saadetsimsek <saadetsimsek13@gmail.com>
2025-01-03 17:14:26 +0300

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Saadet Şimşek on 03/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TimerTests/TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerTests.swift
+//  TimerTests
+//
+//  Created by Saadet Şimşek on 03/01/2025.
+//
+
+import Testing
+@testable import Timer
+
+struct TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
